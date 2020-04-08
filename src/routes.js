632b4165ab121129fc3eb168f547d68caef13c0d
EsioFreitas <esio.gustavo@gmail.com>
2020-04-08 09:44:03 -0300

    Connect api with database

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
 
+import User from "./app/models/User";
+
 const routes = new Router();
 
-routes.get("/", (req, res) => res.json({ message: "Hello World" }));
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "esio",
+    email: "esio@esio.com",
+    passoword_hash: "123456789",
+  });
+  return res.json(user);
+});
 
 export default routes;
