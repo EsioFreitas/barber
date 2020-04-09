552cf4ad0c7ef84842d7bde2d95f704b893dc268
EsioFreitas <esio.gustavo@gmail.com>
2020-04-09 10:32:37 -0300

    Add auth middleware

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -25,6 +25,11 @@ class SessionController {
       }),
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
 }
 
 export default new SessionController();
